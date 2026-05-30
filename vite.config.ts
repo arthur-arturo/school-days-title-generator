66f903b62ae3af32fdbd75858607003121a5868e
Arthur <acallomamani@uade.edu.ar>
2026-05-30 02:56:36 -0300

    Add gh-pages deployment configuration and scripts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/school-days-title-generator/",
   plugins: [
     react(),
     tailwindcss()
